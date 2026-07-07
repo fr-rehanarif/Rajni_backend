b81c8f5989d9a495bb9ca820da3630f241416673
Rehan <rehanarif.mohammad@gmail.com>
2026-07-07 20:37:21 +0530

    Added customer API

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use("/api/auth", authRoutes);
+app.use("/api/customers", customerRoutes);
 app.use("/api/products", productRoutes);
 app.use("/api/sales", salesRoutes);
 
