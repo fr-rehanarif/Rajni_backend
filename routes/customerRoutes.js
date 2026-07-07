7ebbb5c5e7bcd0af22d440e6e45873f62acce87c
Rehan <rehanarif.mohammad@gmail.com>
2026-07-07 22:03:13 +0530

    Added customer API 2

--- routes/customerRoutes.js
+++ routes/customerRoutes.js
@@ -4,33 +4,23 @@ const protect = require("../middleware/authMiddleware");
 const {
   getCustomers,
   getCustomer,
-  createCustomer,
+  addCustomer,
   updateCustomer,
   deleteCustomer,
-  searchCustomers,
 } = require("../controllers/customerController");
 
 const router = express.Router();
 
-// All routes protected
 router.use(protect);
 
-// Get all customers
 router.get("/", getCustomers);
 
-// Search customer
-router.get("/search", searchCustomers);
-
-// Get single customer
 router.get("/:id", getCustomer);
 
-// Create customer
-router.post("/", createCustomer);
+router.post("/", addCustomer);
 
-// Update customer
 router.put("/:id", updateCustomer);
 
-// Delete customer
 router.delete("/:id", deleteCustomer);
 
 module.exports = router;
