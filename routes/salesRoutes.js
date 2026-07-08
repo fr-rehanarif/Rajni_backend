f161ed7b53265a887584229616f18134eb2a3842
Rehan <rehanarif.mohammad@gmail.com>
2026-07-09 01:26:57 +0530

    Added BILLING UPDATE

--- routes/salesRoutes.js
+++ routes/salesRoutes.js
@@ -1,5 +1,5 @@
 const express = require("express");
-
+const pool = require("../config/db");
 const {
   createSale,
   getSales,
@@ -11,6 +11,37 @@ const {
 const authMiddleware = require("../middleware/authMiddleware");
 
 const router = express.Router();
+
+router.get("/customer/:mobile", authMiddleware, async (req, res) => {
+  try {
+    const { mobile } = req.params;
+
+    const result = await pool.query(
+      "SELECT * FROM customers WHERE mobile = $1",
+      [mobile]
+    );
+
+    if (result.rows.length === 0) {
+      return res.json({
+        success: false,
+        customer: null,
+      });
+    }
+
+    res.json({
+      success: true,
+      customer: result.rows[0],
+    });
+
+  } catch (err) {
+    console.error(err);
+
+    res.status(500).json({
+      success: false,
+      message: "Server Error",
+    });
+  }
+});
 
 // Create Bill
 router.post(
